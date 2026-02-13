a9e1d794cf2faeee1b6bfe5cca6738e874b32f2d
Lu Lan <lucialanburilloblazquez@gmail.com>
2026-02-13 16:49:06 +0100

    Actualizar leccion1.py

--- leccion1.py
+++ leccion1.py
@@ -5,3 +5,10 @@ estudiante = True      # Un valor de verdad (Boolean)
 
 # Para ver el resultado en pantalla usamos print()
 print(f"Hola, mi nombre es {nombre} y tengo {edad} años. ❤️")
+
+precio_original = 100
+descuento_porcentaje = 0.10  # Esto representa un 10%
+cantidad_descontada = precio_original * descuento_porcentaje
+precio_final = precio_original - cantidad_descontada
+
+print(f"El precio final del curso es: {precio_final} euros. ❤️")
