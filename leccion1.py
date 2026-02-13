e230bfa3452ebc96e105abc8cd9414b6fbf5088c
Lu Lan <lucialanburilloblazquez@gmail.com>
2026-02-13 17:32:41 +0100

    Actualizar leccion1.py

--- leccion1.py
+++ leccion1.py
@@ -9,9 +9,3 @@ print(f"Hola, mi nombre es {nombre} y tengo {edad} años. ❤️")
 precio_original = 100
 descuento = precio_original * 0.10
 precio_final = precio_original - descuento
-
-# La magia para escribir en el README.md
-mensaje = f"# Resultado del Curso\n\nEl precio final calculado es: **{precio_final} euros**. ❤️"
-
-with open("README.md", "w") as archivo:
-    archivo.write(mensaje)
