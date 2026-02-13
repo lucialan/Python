ea68326ac6f649e77a5acdaf524accf3bed01b45
Lu Lan <lucialanburilloblazquez@gmail.com>
2026-02-13 16:06:55 +0100

    Crear leccion1.py

---
+++ leccion1.py
@@ -0,0 +1,7 @@
+# Esto es un comentario, no hace nada, es para nosotros
+nombre = "Lu"          # Un texto (String)
+edad = 25              # Un número entero (Integer)
+estudiante = True      # Un valor de verdad (Boolean)
+
+# Para ver el resultado en pantalla usamos print()
+print(f"Hola, mi nombre es {nombre} y tengo {edad} años. ❤️")
